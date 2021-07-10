7ec3067c997f421dcce15fb77fc4f4149813b498
GustavoGomesDias <treeelevengg@gmail.com>
2021-07-10 01:43:03 -0300

    feat: add filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Expenses.css';
 
 import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 
 function Expenses(props) {
+  const [year, setYear] = useState('');
+
+  const filterChangeHandler = (year) => {
+    setYear(year);
+  }
+
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={year}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem expenses={props.items[0]} />
       <ExpenseItem expenses={props.items[1]} />
       <ExpenseItem expenses={props.items[2]} />
