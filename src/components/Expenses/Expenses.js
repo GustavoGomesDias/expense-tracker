e4b0dd1d9d9d0eebd3238fad09e16c3655265108
GustavoGomesDias <treeelevengg@gmail.com>
2021-07-11 19:30:09 -0300

    feat: add filtered option

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,18 +12,30 @@ function Expenses(props) {
     setYear(year);
   }
 
+  /* My Solution */
+  // const filterExpenses = () => {
+  //   const arr = [];
+  //   props.items.map((expense) => (expense.date.getFullYear().toString() === year ? arr.push(expense) : arr));
+
+  //   return arr;
+  // }
+
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === year;
+  });
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={year}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) =>
-        <ExpenseItem
-          key={expense.id}    
-          expenses={expense}
-        />
-      )}
+      {(year !== '' ? filterExpenses : props.items).map((expense) =>
+          <ExpenseItem
+            key={expense.id}
+            expenses={expense}
+          />
+        )}
     </Card>
   );
 }
