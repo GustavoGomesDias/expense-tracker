df4c518c9bf3ec0d1979b2d2aa05aa58e3151d00
GustavoGomesDias <treeelevengg@gmail.com>
2021-07-11 02:04:22 -0300

    feat: add new expenses em screen

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New Tv',
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  }
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New Tv',
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    }
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    // prevExpenses é o estado anteiror de expenses, usamos ele para adicionar as outras despesas no array novo que está sendo setado como novo estado de expenses
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
   };
 
   return (
